a1a4e56dae5667f1d063a915236fbad4c1f3be10
Nitish Singh <nitman118@gmail.com>
2019-03-05 14:04:42 +0100

    added hw1

---
+++ pirple-hw1/hw1_pirple.js
@@ -0,0 +1,39 @@
+/*
+This is HW 1 of pirple.com Keeping up with javascript ES6.
+*/
+
+
+//name of song
+const songName = 'Take on me';
+console.log(`Song Name:${songName}`);
+//artist
+const artist = 'a-ha';
+console.log(`Artist Name:${artist}`);
+
+//Album
+const album = 'Love is Reason';
+console.log(`Album Name:${album}`);
+
+//youtube link
+const link ='https://www.youtube.com/watch?v=djV11Xbc914';
+console.log(`Youtube Link:${link}`);
+
+//band details
+let band ={
+    singers:['Magne','Morten','Paul'],
+    keyboard:'Magne',
+    guitar:'Paul',
+};
+
+console.log(`Band Object:${band}`);
+console.log(`Singers:${band.singers}`)
+console.log(`Keyboard:${band.keyboard}`)
+console.log(`Guitarist:${band.guitar}`)
+
+//genre of song
+const genre = 'classic rock';
+console.log(`Song genre:${genre}`);
+
+// is awesome?
+const isAwesome = true;
+console.log(`Is it awesome?:${isAwesome}`);
